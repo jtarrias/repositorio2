44db36acacce358f664c51bf6901ef7b7324e623
jtarr <jtarr@DESKTOP-9TNMTBQ>
2019-03-11 09:49:47 +0100

    Array 10 elementos

---
+++ src/repositorio2/operararrays.java
@@ -0,0 +1,15 @@
+package repositorio2;
+
+public class operararrays {
+	public static int[] rellenaArrays(int x[]) {
+		for (int i = 0; i < x.length; i++) {
+			x[i] = (int) Math.round(Math.random() * 20);
+		}
+		return x;
+	}
+
+	public static void main(String[] args) {
+		int array[] = new int[10];
+		rellenaArrays(array);
+	}
+}
